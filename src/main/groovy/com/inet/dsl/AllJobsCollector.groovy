f8f373efe92041b1d354d5457425337e61dbe699
Gerry Weißbach <gerry.w@gammaproduction.de>
2016-02-25 10:47:45 +0100

    Update to AllJobsCollector because the JobDSL runs on the master while the script parts run on the slave (that is why we use the master node now).  We have to write the Jobs that we want in the Build All to a serialized file to keep track of them.

--- src/main/groovy/com/inet/dsl/AllJobsCollector.groovy
+++ src/main/groovy/com/inet/dsl/AllJobsCollector.groovy
@@ -1,25 +1,74 @@
 package com.inet.dsl
 
 /**
- * Collects all Jobs in a list
- * Can later be used to generate a build job with all configred jobs in it
- */ 
+ * Collects all Jobs so that they can be put into the Build All job
+ */
 class AllJobsCollector {
     
-    private static Map<JobType,List<String>> jobList = new HashMap()
+    /**
+     * Set up an empty jobs.list file on startup
+     */
+    private static jobFile = new File('/tmp/jobs.list')
+    static {
+        try {
+            
+            // This will only compile in the jenkins groovy environment as we do not provide the jenkins libs!
+            hudson.FilePath workspace = hudson.model.Executor.currentExecutor().getCurrentWorkspace()
+            if ( !workspace.isRemote() ) {
+                jobFile = new File( workspace.getRemote(), "jobs.list" )
+            }
+        } catch( Exception e ){}
+    }
     
+    /**
+     * Add a job to the list
+     * @param name - name of the job
+     * @param priority - Type/priority
+     */
     public static addJob( String name, JobType priority ) {
         
-        List<String> jobs = jobList.get( priority )
+        HashMap jobList = getJobList();
+        List<String> jobs = jobList.get( priority.toString() )
         if ( !jobs ) {
             jobs = new ArrayList()
         }
         
         jobs.add( name )
-        jobList.put( priority, jobs )
+        jobList.put( priority.toString(), jobs )
+
+        writeJobList(jobList);
     }
     
-    public static Map<JobType,List<String>> getJobList() {
-        return jobList
+    /**
+     * Read the job list or create a new one
+     * Will crash if file errors appear. We do not catch them
+     * because we do not have a PrintStream.
+     * @retuns List of Job in Map
+     */
+    public static HashMap getJobList() {
+        
+        HashMap map = new HashMap();
+        try {
+            FileInputStream fis = new FileInputStream(jobFile);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            map = (HashMap) ois.readObject();
+            ois.close();
+            fis.close();
+        } catch ( IOException e ) {}
+        return map
+    }
+    
+    /**
+     * Write the job list or create a new one
+     * Will crash if file errors appear. We do not catch them
+     * because we do not have a PrintStream.
+     * @param jobList - List of Job in Map
+     */
+    private static writeJobList(HashMap jobList) {
+        FileOutputStream fos = new FileOutputStream(jobFile);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(jobList);
+        oos.close();
+        fos.close();
     }
 }
